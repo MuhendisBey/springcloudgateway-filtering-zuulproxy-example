973438e2beaf742b1906096daf860b8b7fbf4938
Onur GURSOY <onurgursoy@mail.com>
2021-10-18 23:24:39 +0300

    second bean override existing filter

--- springcloudgateway/src/main/java/com/muhendisbey/springcloudgatewayzuulproxyexample/springcloudgateway/config/filter/SamplePreFilterFactory.java
+++ springcloudgateway/src/main/java/com/muhendisbey/springcloudgatewayzuulproxyexample/springcloudgateway/config/filter/SamplePreFilterFactory.java
@@ -2,13 +2,22 @@ package com.muhendisbey.springcloudgatewayzuulproxyexample.springcloudgateway.co
 
 import com.muhendisbey.springcloudgatewayzuulproxyexample.springcloudgateway.lib.PreFilter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class SamplePreFilterFactory
 {
     @Bean
     PreFilter SamplePreFilter()
     {
-        return new PreFilter( (exchange, chain) -> System.out.println("PRE: ex" + exchange + "ch" + chain)
+        return new PreFilter( (exchange, chain) -> System.out.println("SamplePreFilter: ex" + exchange + "ch" + chain)
+        );
+    }
+
+    @Bean /*Note: Onur: This bean overrides above bean */
+    PreFilter ExamplePreFilter()
+    {
+        return new PreFilter( (exchange, chain) -> System.out.println("ExamplePreFilter: ex" + exchange + "ch" + chain)
         );
     }
 }
