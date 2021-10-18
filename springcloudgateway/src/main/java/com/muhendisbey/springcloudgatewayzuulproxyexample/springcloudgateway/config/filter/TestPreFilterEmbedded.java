7f8cab3e5a17f4ba5525f3e41883c223d7d3bb80
Onur GURSOY <onurgursoy@mail.com>
2021-10-18 23:24:06 +0300

    i couldnt figure out the make generic prefilter

---
+++ springcloudgateway/src/main/java/com/muhendisbey/springcloudgatewayzuulproxyexample/springcloudgateway/config/filter/TestPreFilterEmbedded.java
@@ -0,0 +1,27 @@
+package com.muhendisbey.springcloudgatewayzuulproxyexample.springcloudgateway.config.filter;
+
+import com.muhendisbey.springcloudgatewayzuulproxyexample.springcloudgateway.lib.PreFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+/*TODO: This is incpompleted. this created new filter with new name */
+@Component
+public class TestPreFilterEmbedded //extends PreFilter
+{
+    private final Logger _logger = LoggerFactory.getLogger(TestPreFilterEmbedded.class);
+
+    public TestPreFilterEmbedded()
+    {
+        super();
+//        super(this::preFilter);
+
+    }
+
+    public void preFilter(ServerWebExchange serverWebExchange, GatewayFilterChain gatewayFilterChain)
+    {
+        _logger.info("TestPreFilterEmbbeded preFilterHandler was called!");
+    }
+}
